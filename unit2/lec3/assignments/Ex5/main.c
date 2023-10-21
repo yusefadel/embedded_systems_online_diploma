633c70c65dfdc9a403980621fe1c7fbbf5c7f737
joadel <youssifadel2222@gmail.com>
2023-10-21 22:22:20 +0200

    update in unit 2 lec 3 assignments Ex5

---
+++ unit2/lec3/assignments/Ex5/main.c
@@ -0,0 +1,17 @@
+
+#include"stdio.h"
+int main(){
+	char x;
+	printf("##########Console-output######\n");
+	printf("Enter a character: ");
+	fflush(stdout);
+	scanf("%c",&x);
+	printf("ASCII value of G = %d\n",x);
+	printf("#############################\n\n");
+	printf("##############################################################"
+			"\n##########");
+
+	return 0;
+}
+
+
