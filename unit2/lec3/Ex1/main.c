fb4e9e1c5fcac47870ba124f4554afc8f82916d1
joadel <youssifadel2222@gmail.com>
2023-10-21 20:50:11 +0200

    unit 2 lec 3 assignments Ex1 only

---
+++ unit2/lec3/Ex1/main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 12, 2023
+ *      Author: FreeComp
+ */
+#include"stdio.h"
+int main(){
+	printf("##########Console-output###\nC Programming\n\n"
+			"##############################################################"
+			"\n##########");
+	return 0;
+}
+
