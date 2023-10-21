f0b2f73e80e0f5a22b7fe15e85aac375c25c2aaf
joadel <youssifadel2222@gmail.com>
2023-10-21 22:09:25 +0200

    update unit 2 lec3 assignments Ex3

---
+++ unit2/lec3/assignments/Ex3/main.c
@@ -0,0 +1,21 @@
+
+#include"stdio.h"
+int main(){
+	int x;
+	int y;
+	printf("##########Console-output###\n");
+	printf("Enter two integers: \n");
+	fflush(stdout);
+	scanf("%d",&x);
+	fflush(stdout);
+	scanf("%d",&y);
+
+	printf("Sum: %d\n",x+y);
+	printf("###########################\n\n"
+			"##############################################################"
+			"\n##########");
+
+	return 0;
+}
+
+
